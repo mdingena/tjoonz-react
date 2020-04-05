d14ff1bc14361001fa7f815807868882806d9caf
Marc Dingena <marc.dingena@gmail.com>
2020-04-05 14:37:12 +1000

    Setup workspace

---
+++ src/providers/ResizeObserver.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from '@envato/react-breakpoints';
+import { ResizeObserver } from '@juggle/resize-observer';
+
+const ResizeObserverProvider = ({ children }) => (
+  <Provider ponyfill={ResizeObserver}>
+    {children}
+  </Provider>
+);
+
+ResizeObserverProvider.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default ResizeObserverProvider;
