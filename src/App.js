a3c96edf8b2d11475f438ad6e5a97d7a2c3dced0
Marc Dingena <marc.dingena@gmail.com>
2020-04-08 21:08:07 +1000

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import StoreProvider from './providers/StoreProvider';
 import ResizeObserverProvider from './providers/ResizeObserverProvider';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 const App = () => (
   <StoreProvider>
     <ResizeObserverProvider>
-      <div className='app' />
+      <Router>
+        <nav>Nav</nav>
+
+        <Routes>
+          <Route path='/' element={<div>Home <Link to='listen'>Listen</Link></div>} />
+          <Route path='listen' element={<div>Listen <Link to='/'>Back home</Link></div>} />
+        </Routes>
+
+        <div className='player'>Player</div>
+      </Router>
     </ResizeObserverProvider>
   </StoreProvider>
 );
