a2ac1c14dd92012cb58be22431281d202e1e6406
Marc Dingena <marc.dingena@gmail.com>
2020-04-09 23:01:51 +1000

    Add Redux store to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { addDecorator, addParameters } from '@storybook/react';
 import { themes } from '@storybook/theming';
+import { StoreProvider } from '../src/providers/StoreProvider';
 import { ResizeObserverProvider } from '../src/providers/ResizeObserverProvider';
 import { MemoryRouter } from 'react-router-dom';
 import '../src/index.css';
 
+addDecorator(storyFn => <StoreProvider>{storyFn()}</StoreProvider>);
 addDecorator(storyFn => <ResizeObserverProvider>{storyFn()}</ResizeObserverProvider>);
 addDecorator(storyFn => <MemoryRouter>{storyFn()}</MemoryRouter>);
 
