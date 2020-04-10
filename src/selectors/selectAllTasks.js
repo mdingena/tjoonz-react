389b43b552dba45da304ff57fbcdb58173cf6577
Marc Dingena <marc.dingena@gmail.com>
2020-04-11 08:19:09 +1000

    Also return completed tasks

--- src/selectors/selectAllTasks.js
+++ src/selectors/selectAllTasks.js
@@ -1,4 +1,6 @@
-const selectAllTasks = ({ tasks }) =>
-  Object.values(tasks.queued).reduce((sum, taskCount) => sum + taskCount, 0);
+const selectAllTasks = ({ tasks }) => ({
+  completed: tasks.completed,
+  queued: Object.values(tasks.queued).reduce((sum, taskCount) => sum + taskCount, 0)
+});
 
 export default selectAllTasks;
