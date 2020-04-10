5719b641d9069f39595d6faa3a8f7327be3ec9b6
Marc Dingena <marc.dingena@gmail.com>
2020-04-11 07:49:25 +1000

    Add tasks reducer

--- src/reducers/TasksReducer.js
+++ src/reducers/TasksReducer.js
@@ -1,11 +1,17 @@
-import { ADD_TASKS } from '../constants/actionTypes';
+import { ADD_TASKS, DECREASE_TASKS } from '../constants/actionTypes';
 
 const TasksReducer = (state = {}, { type, payload }) => {
   switch (type) {
     case ADD_TASKS:
       return {
         ...state,
-        [payload.key]: payload.count + (state[payload.key] ? state[payload.key].count : 0)
+        [payload.key]: payload.count + (state[payload.key] || 0)
+      };
+
+    case DECREASE_TASKS:
+      return {
+        ...state,
+        [payload.key]: Math.max((state[payload.key] || 0) - payload.count, 0)
       };
 
     default:
