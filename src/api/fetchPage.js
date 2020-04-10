23fdccf9770d252f6403cb9187e8a45807dba0a4
Marc Dingena <marc.dingena@gmail.com>
2020-04-10 15:46:44 +1000

    Add fetchPage function

---
+++ src/api/fetchPage.js
@@ -0,0 +1,29 @@
+import qs from 'qs';
+import api from '../constants/api';
+
+const defaultOptions = {
+  per_page: api.ITEMS_PER_PAGE
+};
+
+/**
+ * Fetch a page of the WP API resource at `endpoint`.
+ * @param {String} endpoint - Name of the WP API resource.
+ * @param {Number} page - Page number to fetch.
+ * @param {Object} [options] - Sets options for fetching, such as sorting and results per page.
+ */
+const fetchPage = async (endpoint, page, options = {}) => {
+  /* Construct a query string for fetching the resource. */
+  const query = qs.stringify({ ...defaultOptions, ...options, page });
+
+  /* Fetch a page. */
+  const response = await window.fetch(`${api.URL}${endpoint}${query}`);
+
+  /* Extract data. */
+  const pageCount = response.headers.get('X-WP-TotalPages');
+  const resources = await response.json();
+
+  /* Return results. */
+  return { resources, pageCount };
+};
+
+export default fetchPage;
---
+++ src/constants/api.js
@@ -0,0 +1,5 @@
+export default {
+  URL: 'https://www.tjoonz.com/wp-json/wp/v2',
+  MIXES: '/posts',
+  ITEMS_PER_PAGE: 100
+};
