1c8219744512a28695f9b89ef2577a305743bf82
Marc Dingena <marc.dingena@gmail.com>
2020-04-10 16:48:24 +1000

    Update API constants

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,7 @@
 export default {
-  URL: 'https://www.tjoonz.com/wp-json/wp/v2',
-  MIXES: '/posts',
+  BASE_URL: 'https://www.tjoonz.com/wp-json/wp/v2',
+  ENDPOINTS: {
+    MIXES: '/posts'
+  },
   ITEMS_PER_PAGE: 100
 };
