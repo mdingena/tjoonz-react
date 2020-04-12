7ff95402478f7f3b5287eb1087cc8e559dee8bc8
Marc Dingena <marc.dingena@gmail.com>
2020-04-13 01:13:59 +1000

    Update Navigation stories

--- src/components/LoadingSpinner/LoadingSpinner.story.js
+++ src/components/LoadingSpinner/LoadingSpinner.story.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import addTasks from '../../actions/addTasks';
 import completeTasks from '../../actions/completeTasks';
--- src/components/Navigation/Navigation.story.js
+++ src/components/Navigation/Navigation.story.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import addTasks from '../../actions/addTasks';
+import completeTasks from '../../actions/completeTasks';
 import Navigation from './Navigation';
 
 export default {
@@ -6,17 +9,35 @@ export default {
   component: Navigation
 };
 
+const Wrapper = ({ children }) => {
+  const dispatch = useDispatch();
+  const add5 = () => dispatch(addTasks('foo', 5));
+  const complete1 = () => dispatch(completeTasks('foo', 1));
+
+  return (
+    <>
+      <div style={{ padding: 50 }}>
+        {children}
+      </div>
+      <button onClick={add5}>Add 5 tasks</button>
+      <button onClick={complete1}>Complete 1 task</button>
+    </>
+  );
+};
+
 export const navigation = () => (
-  <Navigation
-    links={[
-      { to: 'listen', text: 'Listen' },
-      { to: 'charts', text: 'Charts' },
-      { to: 'about', text: 'About Tjoonz' },
-      { to: 'about/uploading', text: 'About Uploading' },
-      { to: 'about/contributing', text: 'Become an Editor' },
-      { to: 'thanks', text: 'Special Thanks' },
-      { to: 'privacy-policy', text: 'Privacy Policy' },
-      { to: 'about/feedback', text: 'Feedback' }
-    ]}
-  />
+  <Wrapper>
+    <Navigation
+      links={[
+        { to: 'listen', text: 'Listen' },
+        { to: 'charts', text: 'Charts' },
+        { to: 'about', text: 'About Tjoonz' },
+        { to: 'about/uploading', text: 'About Uploading' },
+        { to: 'about/contributing', text: 'Become an Editor' },
+        { to: 'thanks', text: 'Special Thanks' },
+        { to: 'privacy-policy', text: 'Privacy Policy' },
+        { to: 'about/feedback', text: 'Feedback' }
+      ]}
+    />
+  </Wrapper>
 );
