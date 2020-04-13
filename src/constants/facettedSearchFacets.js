303903e0ea5bbaf985ec99dea46e377fe6204f47
Marc Dingena <marc.dingena@gmail.com>
2020-04-13 14:32:28 +1000

    Add names

--- src/constants/facettedSearchFacets.js
+++ src/constants/facettedSearchFacets.js
@@ -1,14 +1,20 @@
 export const ARTISTS = {
   KEY: 'ARTISTS',
-  ENDPOINT: '/artist'
+  ENDPOINT: '/artist',
+  NAME_SINGULAR: 'artist',
+  NAME_PLURAL: 'artists'
 };
 
 export const GENRES = {
   KEY: 'GENRES',
-  ENDPOINT: '/genre'
+  ENDPOINT: '/genre',
+  NAME_SINGULAR: 'genre',
+  NAME_PLURAL: 'genres'
 };
 
 export const TAGS = {
   KEY: 'TAGS',
-  ENDPOINT: '/tags'
+  ENDPOINT: '/tags',
+  NAME_SINGULAR: 'tag',
+  NAME_PLURAL: 'tags'
 };
