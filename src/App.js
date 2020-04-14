41a98a844e3543809e2d6a9b497a59b4ec66b47a
Marc Dingena <marc.dingena@gmail.com>
2020-04-15 08:50:16 +1000

    Shift resposibility for constraining components

--- src/App.js
+++ src/App.js
@@ -7,31 +7,37 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import Wrap from './components/Wrap';
 import Navigation from './components/Navigation';
+import styles from './App.module.css';
 
 const App = () => (
   <StoreProvider>
     <ResizeObserverProvider>
       <Router>
-        <Navigation
-          links={[
-            { to: 'listen', text: 'Listen' },
-            { to: 'charts', text: 'Charts' },
-            { to: 'test1', text: 'Test 1' },
-            { to: 'test2', text: 'Test 2' },
-            { to: 'test3', text: 'Test 3' },
-            { to: 'test4', text: 'Test 4' },
-            { to: 'test5', text: 'Test 5' },
-            { to: 'test6', text: 'Test 6' },
-            { to: 'test7', text: 'Test 7' },
-            { to: 'test8', text: 'Test 8' }
-          ]}
-        />
 
         <Routes>
           <Route path='/' element={<div>Home <Link to='listen'>Listen</Link></div>} />
           <Route path='listen' element={<div>Listen <Link to='/'>Back home</Link></div>} />
         </Routes>
+        <div className={styles.navigation}>
+          <Wrap>
+            <Navigation
+              links={[
+                { to: 'listen', text: 'Listen' },
+                { to: 'charts', text: 'Charts' },
+                { to: 'test1', text: 'Test 1' },
+                { to: 'test2', text: 'Test 2' },
+                { to: 'test3', text: 'Test 3' },
+                { to: 'test4', text: 'Test 4' },
+                { to: 'test5', text: 'Test 5' },
+                { to: 'test6', text: 'Test 6' },
+                { to: 'test7', text: 'Test 7' },
+                { to: 'test8', text: 'Test 8' }
+              ]}
+            />
+          </Wrap>
+        </div>
 
         <div className='player'>Player</div>
       </Router>
