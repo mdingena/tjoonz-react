c34178b519d433a73833fe9f1098f925dade0dcf
Marc Dingena <marc.dingena@gmail.com>
2020-04-16 14:29:33 +1000

    Update button component API

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,25 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-const Button = ({ onClick, text, Icon = () => null, disabled = false }) => (
-  <button
-    className={styles.root}
-    onClick={onClick}
-    disabled={disabled}
-    type='button'
-  >
-    <Icon className={styles.icon} />
-    <span className={styles.text}>
-      {text}
-    </span>
-  </button>
-);
+const Button = ({
+  onClick,
+  text,
+  Icon = () => null,
+  disabled = false,
+  size = 'small',
+  align = 'center'
+}) => {
+  const className = [styles.root, styles[size], styles[align]].join(' ');
+
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+      disabled={disabled}
+      type='button'
+    >
+      {align !== 'right' && <Icon className={styles.iconLeft} />}
+      {text && (
+        <span className={styles.text}>
+          {text}
+        </span>
+      )}
+      {align === 'right' && <Icon className={styles.iconRight} />}
+    </button>
+  );
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string,
   Icon: PropTypes.func,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  size: PropTypes.oneOf(['small', 'large']),
+  align: PropTypes.oneOf(['left', 'center', 'right'])
 };
 
 export default Button;
