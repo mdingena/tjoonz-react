09f5ee0a04c47116b0f344720563a9dfe4159ec7
Marc Dingena <marc.dingena@gmail.com>
2020-04-21 12:19:54 +1000

    Remove facetted search relation toggle
    Until I fix the WP REST API code that handles AND/OR relations.

--- src/components/FacettedSearchHeader/FacettedSearchHeader.js
+++ src/components/FacettedSearchHeader/FacettedSearchHeader.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import selectFacettedSearchFacet from '../../selectors/selectFacettedSearchFacet';
 import { FACET_PROPTYPES } from '../../constants/facettedSearchFacets';
-import { OR } from '../../constants/facettedSearchRelations.js';
-import toggleFacettedSearchRelation from '../../actions/toggleFacettedSearchRelation';
-import Icon from '../Icon';
 import styles from './FacettedSearchHeader.module.css';
 
 const FacettedSearchHeader = ({ facet }) => {
-  const dispatch = useDispatch();
-  const { selected, relation } = useSelector(selectFacettedSearchFacet(facet));
-
-  const handleToggle = () => {
-    const action = toggleFacettedSearchRelation(facet);
-    dispatch(action);
-  };
+  const { selected } = useSelector(selectFacettedSearchFacet(facet));
 
   return (
     <div className={styles.root}>
@@ -22,15 +13,6 @@ const FacettedSearchHeader = ({ facet }) => {
         <span className={styles.facet}>{facet.NAME_PLURAL}</span>
         {selected.length > 0 && <span className={styles.selected}>{selected.length}</span>}
       </div>
-      <button
-        className={styles.toggle}
-        onClick={handleToggle}
-        type='button'
-      >
-        Any
-        <Icon.ToggleOn className={relation === OR ? styles.matchAny : styles.matchAll} />
-        All
-      </button>
     </div>
   );
 };
