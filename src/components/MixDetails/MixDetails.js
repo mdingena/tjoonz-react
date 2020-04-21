7ecb67b02069260746e523acdbb70c30a71397ac
Marc Dingena <marc.dingena@gmail.com>
2020-04-21 17:14:42 +1000

    Add empty state

--- src/components/MixDetails/MixDetails.js
+++ src/components/MixDetails/MixDetails.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import styles from './MixDetails.module.css';
 
 const MixDetails = ({
+  empty,
   slug,
   thumbnail,
   poster,
@@ -32,57 +33,85 @@ const MixDetails = ({
   return (
     <div className={styles.root}>
       <div className={posterRevealed ? styles.posterRevealed : styles.posterLoading}>
-        <img src={thumbnail} alt={title} />
-        <img src={poster} alt={title} onLoad={handlePosterLoaded} />
+        {!empty
+          ? (
+            <>
+              <img src={thumbnail} alt={title} />
+              <img src={poster} alt={title} onLoad={handlePosterLoaded} />
+            </>
+          )
+          : (
+            <div className={styles.logo}>
+              <div>
+                <div />
+                <div />
+              </div>
+            </div>
+          )}
       </div>
-      <div className={styles.controls}>
-        <Button
-          onClick={() => console.log('play')}
-          text='Play'
-          Icon={Icon.Play}
-        />
-        <Button
-          onClick={() => console.log('queue')}
-          text='Queue'
-          Icon={Icon.Square}
-        />
-        <Button
-          onClick={() => console.log('download')}
-          text='Download'
-          Icon={Icon.CloudDownload}
-        />
-      </div>
-      <Link to={`/mix/${slug}/`} className={styles.link}>
-        <Icon.ListOl className={styles.icon} />
-        <span className={styles.text}>Tracklist and comments</span>
-      </Link>
-      <div className={styles.details}>
-        <div>Published</div>
-        <div>{published}</div>
-        <div>Artists</div>
-        <div>{artists}</div>
-        <div>Title</div>
-        <div>{title}</div>
-        <div>Genres</div>
-        <div>{genres}</div>
-        <div>Tags</div>
-        <div>{tags}</div>
-        <div>Duration</div>
-        <div>{duration}</div>
-      </div>
+      {!empty && (
+        <div className={styles.controls}>
+          <Button
+            onClick={() => console.log('play')}
+            text='Play'
+            Icon={Icon.Play}
+          />
+          <Button
+            onClick={() => console.log('queue')}
+            text='Queue'
+            Icon={Icon.Square}
+          />
+          <Button
+            onClick={() => console.log('download')}
+            text='Download'
+            Icon={Icon.CloudDownload}
+          />
+        </div>
+      )}
+      {!empty && (
+        <Link to={`/mix/${slug}/`} className={styles.link}>
+          <Icon.ListOl className={styles.icon} />
+          <span className={styles.text}>Tracklist and comments</span>
+        </Link>
+      )}
+      {!empty && (
+        <div className={styles.details}>
+          <div>Published</div>
+          <div>{published}</div>
+          <div>Artists</div>
+          <div>{artists}</div>
+          <div>Title</div>
+          <div>{title}</div>
+          <div>Genres</div>
+          <div>{genres}</div>
+          <div>Tags</div>
+          <div>{tags}</div>
+          <div>Duration</div>
+          <div>{duration}</div>
+        </div>
+      )}
       <div className={styles.description}>
-        {description}
+        {!empty
+          ? description
+          : (
+            <>
+              <h1>Tjoonz.com</h1>
+              <p>An EDM mixtape archive launched in 2008, run entirely in our spare time and out of our own pockets.</p>
+            </>
+          )}
       </div>
-      <div className={styles.meta}>
-        <div>Plays</div>
-        <div>{plays}</div>
-        <div>Downloads</div>
-        <div>{downloads}</div>
-        <div>Quality</div>
-        <div>{quality} <small>kbps</small></div>
-        <div>File size</div>
-        <div>{fileSize} <small>MB</small></div>
-      </div>
+      {!empty && (
+        <div className={styles.meta}>
+          <div>Plays</div>
+          <div>{plays}</div>
+          <div>Downloads</div>
+          <div>{downloads}</div>
+          <div>Quality</div>
+          <div>{quality} <small>kbps</small></div>
+          <div>File size</div>
+          <div>{fileSize} <small>MB</small></div>
+        </div>
+      )}
     </div>
   );
 };
