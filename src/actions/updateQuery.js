cfac4ab608b4dfbe5af7ff7a1eaf4e17b93e1f52
Marc Dingena <marc.dingena@gmail.com>
2020-04-21 14:58:54 +1000

    Add update query action creator

---
+++ src/actions/updateQuery.js
@@ -0,0 +1,58 @@
+import {
+  DONE_FETCHING_QUERY_RESULTS,
+  SET_QUERY_RESULTS,
+  START_FETCHING_QUERY_RESULTS
+} from '../constants/actionTypes';
+import { ENDPOINTS } from '../constants/api';
+import { ARTISTS, GENRES, TAGS } from '../constants/facettedSearchFacets';
+import addTasks from '../actions/addTasks';
+import completeTasks from '../actions/completeTasks';
+import fetchPage from '../api/fetchPage';
+import extractMixData from '../api/extractMixData';
+
+const updateQuery = facettedSearch => async dispatch => {
+  dispatch(startFetching(facettedSearch));
+  dispatch(addTasks(SET_QUERY_RESULTS, 1));
+
+  const options = { _embed: true };
+
+  if (facettedSearch[ARTISTS.KEY].selected.length > 0) options[ARTISTS.TAXONOMY] = facettedSearch[ARTISTS.KEY].selected.map(({ id }) => id).join(',');
+  if (facettedSearch[GENRES.KEY].selected.length > 0) options[GENRES.TAXONOMY] = facettedSearch[GENRES.KEY].selected.map(({ id }) => id).join(',');
+  if (facettedSearch[TAGS.KEY].selected.length > 0) options[TAGS.TAXONOMY] = facettedSearch[TAGS.KEY].selected.map(({ id }) => id).join(',');
+
+  console.log(options);
+
+  const response = await fetchPage(ENDPOINTS.MIXES, 1, options);
+
+  if (!response.ok) {
+    dispatch(doneFetching(null, response.statusText));
+    dispatch(completeTasks(SET_QUERY_RESULTS, 1));
+    return;
+  }
+
+  const mixes = response.resources.map(extractMixData);
+
+  dispatch(setQueryResults(mixes));
+
+  const nextPage = response.resources.pageCount > 1 ? 2 : null;
+
+  dispatch(doneFetching(nextPage));
+  dispatch(completeTasks(SET_QUERY_RESULTS, 1));
+};
+
+export default updateQuery;
+
+const startFetching = facettedSearch => ({
+  type: START_FETCHING_QUERY_RESULTS,
+  payload: { facettedSearch }
+});
+
+const doneFetching = (nextPage, statusText = null) => ({
+  type: DONE_FETCHING_QUERY_RESULTS,
+  payload: { nextPage, statusText }
+});
+
+const setQueryResults = results => ({
+  type: SET_QUERY_RESULTS,
+  payload: { results }
+});
