2d7003cc5d40ab60b4bec6151237cbb2283f1862
Marc Dingena <marc.dingena@gmail.com>
2020-04-23 11:40:18 +1000

    Add Audio component

---
+++ src/components/Audio/Audio.js
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react';
+import { useSelector } from 'react-redux';
+import selectPlayer from '../../selectors/selectPlayer';
+import { BASE_STREAM_URL } from '../../constants/api';
+
+const Audio = () => {
+  const ref = useRef();
+
+  const { isPlaying, volumeLevel, playlist, playhead } = useSelector(selectPlayer);
+  const trackAtPlayhead = playlist[playhead];
+
+  const handleBuffering = () => {
+    if (ref.current.buffered.length > 0 && ref.current.duration) {
+      let percentLoaded =
+        ref.current.buffered.end(ref.current.buffered.length - 1) /
+        ref.current.duration;
+      percentLoaded = percentLoaded > 1 ? 1 : percentLoaded;
+
+      console.log('buffering', percentLoaded);
+    }
+  };
+
+  if (ref.current) {
+    const wasPlaying =
+      ref.current.currentTime > 0 &&
+      !ref.current.paused &&
+      !ref.current.ended &&
+      ref.current.readyState > 2;
+
+    if (!isPlaying && wasPlaying) {
+      ref.current.pause();
+    } else if (!wasPlaying) {
+      ref.current.play();
+    }
+  }
+
+  return (
+    <audio
+      ref={ref}
+      preload='metadata'
+      onPlay={() => console.log('onPlay event')}
+      onPause={() => console.log('onPause event')}
+      onProgress={handleBuffering}
+    >
+      {trackAtPlayhead && <source src={`${BASE_STREAM_URL}${trackAtPlayhead.id}`} type='audio/mpeg' />}
+    </audio>
+  );
+};
+
+export default Audio;
