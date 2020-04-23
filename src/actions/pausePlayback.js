96733aa652ccf1b388c39b24df43ae924a598442
Marc Dingena <marc.dingena@gmail.com>
2020-04-23 11:38:45 +1000

    Add play/pause action creators

---
+++ src/actions/pausePlayback.js
@@ -0,0 +1,7 @@
+import { PAUSE_PLAYBACK } from '../constants/actionTypes';
+
+const pausePlayback = () => ({
+  type: PAUSE_PLAYBACK
+});
+
+export default pausePlayback;
---
+++ src/actions/resumePlayback.js
@@ -0,0 +1,15 @@
+import { RESUME_PLAYBACK } from '../constants/actionTypes';
+
+const resumePlayback = () => (dispatch, getState) => {
+  const { player: { playlist, playhead } } = getState();
+
+  const canResume = playlist[playhead];
+
+  if (canResume) {
+    dispatch({
+      type: RESUME_PLAYBACK
+    });
+  }
+};
+
+export default resumePlayback;
