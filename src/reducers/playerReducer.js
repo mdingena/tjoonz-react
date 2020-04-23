9b8336ab6f00af1d80841e29fd1edefa51056200
Marc Dingena <marc.dingena@gmail.com>
2020-04-23 11:39:16 +1000

    Add play/pause action reducers

--- src/reducers/playerReducer.js
+++ src/reducers/playerReducer.js
@@ -1,7 +1,9 @@
 import {
   APPEND_PLAYLIST_ITEMS,
-  START_PLAYBACK,
-  REMOVE_PLAYLIST_ITEMS
+  PAUSE_PLAYBACK,
+  REMOVE_PLAYLIST_ITEMS,
+  RESUME_PLAYBACK,
+  START_PLAYBACK
 } from '../constants/actionTypes';
 
 const initialState = {
@@ -21,12 +23,10 @@ const playerReducer = (state = initialState, { type, payload }) => {
         playlist: [...state.playlist, ...payload.items]
       };
 
-    case START_PLAYBACK:
-      playlist = [...state.playlist.filter(({ id }) => id !== payload.item.id), payload.item];
+    case PAUSE_PLAYBACK:
       return {
         ...state,
-        playlist,
-        playhead: playlist.length - 1
+        isPlaying: false
       };
 
     case REMOVE_PLAYLIST_ITEMS:
@@ -35,6 +35,20 @@ const playerReducer = (state = initialState, { type, payload }) => {
         ...state,
         playlist,
         playhead: playlist.length ? Math.min(playlist.length - 1, state.playhead) : null
+      };
+
+    case RESUME_PLAYBACK:
+      return {
+        ...state,
+        isPlaying: true
+      };
+
+    case START_PLAYBACK:
+      playlist = [...state.playlist.filter(({ id }) => id !== payload.item.id), payload.item];
+      return {
+        ...state,
+        playlist,
+        playhead: playlist.length - 1
       };
 
     default:
