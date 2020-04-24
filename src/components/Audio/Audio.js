81e539a0229bed6eb8dc7b38c5b4e5a13968c2fd
Marc Dingena <marc.dingena@gmail.com>
2020-04-24 22:18:20 +1000

    Update audio component API

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -1,45 +1,97 @@
 import React, { useRef, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import addTasks from '../../actions/addTasks';
+import completeTasks from '../../actions/completeTasks';
 import selectPlayer from '../../selectors/selectPlayer';
 import { BASE_STREAM_URL } from '../../constants/api';
 
-const Audio = () => {
-  const ref = useRef();
+const Audio = ({ isDragging, scrubTo, onBufferProgress, onPlaybackProgress }) => {
+  const dispatch = useDispatch();
+
+  const audioRef = useRef();
+  const isDraggingRef = useRef(false);
+  const isPlayingRef = useRef(false);
   const loadedId = useRef(0);
 
-  const { isPlaying, volumeLevel, playlist, playhead } = useSelector(selectPlayer);
+  const { isPlaying, playlist, playhead } = useSelector(selectPlayer);
   const trackAtPlayhead = playlist[playhead];
 
   const handleBuffering = () => {
-    if (ref.current.buffered.length > 0 && ref.current.duration) {
-      let percentLoaded =
-        ref.current.buffered.end(ref.current.buffered.length - 1) /
-        ref.current.duration;
-      percentLoaded = percentLoaded > 1 ? 1 : percentLoaded;
-
-      console.log('buffering', percentLoaded);
+    if (audioRef.current.buffered.length > 0 && audioRef.current.duration) {
+      let progress = audioRef.current.buffered.end(audioRef.current.buffered.length - 1) / audioRef.current.duration;
+      progress = progress > 1 ? 1 : progress;
+      onBufferProgress(progress);
     }
   };
 
+  const handlePlay = () => {
+    if (audioRef.current) {
+      window.requestAnimationFrame(handlePlaybackProgress);
+    }
+  };
+
+  const handlePlaybackProgress = () => {
+    if (isPlayingRef.current) {
+      if (!isDraggingRef.current && audioRef.current) {
+        const progress = audioRef.current.currentTime / audioRef.current.duration;
+        onPlaybackProgress(progress);
+      }
+
+      window.requestAnimationFrame(handlePlaybackProgress);
+    }
+  };
+
+  const showSpinner = () => {
+    const action = addTasks('audio', 1);
+    dispatch(action);
+  };
+
+  const hideSpinner = () => {
+    const action = completeTasks('audio', 1);
+    dispatch(action);
+  };
+
+  const handleLoadStart = () => showSpinner();
+
+  const handleLoadedData = () => hideSpinner();
+
+  const handleAbort = () => hideSpinner();
+
   useEffect(() => {
-    if (ref.current) {
+    // Set ref'd state, so that our handlePlaybackProgress callback uses the correct state.
+    isPlayingRef.current = isPlaying;
+    isDraggingRef.current = isDragging;
+  }, [isDragging, isPlaying]);
+
+  useEffect(() => {
+    if (audioRef.current.duration) {
+      audioRef.current.currentTime = scrubTo * audioRef.current.duration;
+    }
+  }, [scrubTo]);
+
+  useEffect(() => {
+    if (audioRef.current) {
       if (!trackAtPlayhead) {
-        ref.current.pause();
+        audioRef.current.pause();
       } else {
         const wasPlaying =
-          ref.current.currentTime > 0 &&
-          !ref.current.paused &&
-          !ref.current.ended &&
-          ref.current.readyState > 2;
+          audioRef.current.currentTime > 0 &&
+          !audioRef.current.paused &&
+          !audioRef.current.ended &&
+          audioRef.current.readyState >= 2;
 
         if (!isPlaying && wasPlaying) {
-          ref.current.pause();
+          audioRef.current.pause();
         } else if (isPlaying) {
           if (trackAtPlayhead.id !== loadedId.current) {
-            ref.current.load();
+            /** @todo
+             * Figure out how to avoid interrupting the play() request by a new load request.
+             * https://goo.gl/LdLk22
+            */
+            audioRef.current.load();
             loadedId.current = trackAtPlayhead.id;
           }
-          ref.current.play();
+          audioRef.current.play();
         }
       }
     }
@@ -47,11 +99,13 @@ const Audio = () => {
 
   return (
     <audio
-      ref={ref}
+      ref={audioRef}
       preload='none'
-      onPlay={() => console.log('onPlay event')}
-      onPause={() => console.log('onPause event')}
+      onPlay={handlePlay}
       onProgress={handleBuffering}
+      onLoadStart={handleLoadStart}
+      onLoadedData={handleLoadedData}
+      onAbort={handleAbort}
     >
       {trackAtPlayhead && <source src={`${BASE_STREAM_URL}${trackAtPlayhead.id}`} type='audio/mpeg' />}
     </audio>
