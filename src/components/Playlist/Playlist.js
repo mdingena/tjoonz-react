0ea4c02b5928bdd233d3e6c9157d2e0f1e1c61fb
Marc Dingena <marc.dingena@gmail.com>
2020-04-25 17:17:49 +1000

    Render playlist items

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import Drawer from '../Aside/Drawer';
+import { useSelector } from 'react-redux';
+import selectPlayer from '../../selectors/selectPlayer';
 import { PLAYLIST_DRAWER } from '../../constants/drawers';
+import Drawer from '../Aside/Drawer';
+import Button from '../Button';
+import MixListHeader from '../MixListHeader';
+import MixListItem from '../MixListItem';
 import styles from './Playlist.module.css';
 
-const Playlist = () => (
-  <Drawer drawerKey={PLAYLIST_DRAWER.KEY} align={PLAYLIST_DRAWER.ALIGN}>
-    <div className={styles.root}>Playlist</div>
-  </Drawer>
-);
+const Playlist = () => {
+  const { playlist, playhead } = useSelector(selectPlayer);
+
+  return (
+    <Drawer drawerKey={PLAYLIST_DRAWER.KEY} align={PLAYLIST_DRAWER.ALIGN}>
+      <div className={styles.root}>
+        {playlist.length === 0
+          ? (
+            <Button
+              onClick={() => { /* noop */ }}
+              text='Nothing in playlist'
+              disabled
+            />
+          )
+          : (
+            <>
+              <div className={styles.item}>
+                <MixListHeader />
+              </div>
+              {playlist.map((item, index) => (
+                <div
+                  key={`playlist-${index}`}
+                  className={playhead === index ? styles.active : styles.item}
+                >
+                  <MixListItem shownInPlaylist {...item} />
+                </div>
+              ))}
+            </>
+          )}
+      </div>
+    </Drawer>
+  );
+};
 
 export default Playlist;
