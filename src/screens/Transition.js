a31b35074dbd9f04e71bfef895e8561773453b2a
Marc Dingena <marc.dingena@gmail.com>
2020-04-26 21:05:21 +1000

    Scroll to top on navigation

--- src/screens/Transition.js
+++ src/screens/Transition.js
@@ -14,6 +14,7 @@ const Transition = ({ location, duration = 300, children }) => {
   });
 
   const transitionIn = useCallback(() => {
+    window.scrollTo(0, 0);
     setState(previousState => ({
       ...previousState,
       className: styles.root,
