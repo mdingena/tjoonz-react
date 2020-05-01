031658b056d740c75da886e0825c7338ee23e8bc
Marc Dingena <marc.dingena@gmail.com>
2020-05-02 08:26:10 +1000

    Append to playlist from query and mix states

--- src/actions/appendPlaylistItems.js
+++ src/actions/appendPlaylistItems.js
@@ -1,9 +1,11 @@
 import { APPEND_PLAYLIST_ITEMS } from '../constants/actionTypes';
 
 const appendPlaylistItems = ids => (dispatch, getState) => {
-  const { query } = getState();
+  const { query, mix } = getState();
 
-  const items = query.results.filter(item => ids.includes(item.id));
+  const cache = [...query.results.filter(item => item.id !== mix.current.id), mix.current];
+
+  const items = cache.filter(item => ids.includes(item.id));
 
   dispatch({
     type: APPEND_PLAYLIST_ITEMS,
--- src/actions/startPlayback.js
+++ src/actions/startPlayback.js
@@ -1,9 +1,11 @@
 import { START_PLAYBACK } from '../constants/actionTypes';
 
 const startPlayback = id => (dispatch, getState) => {
-  const { query } = getState();
+  const { query, mix } = getState();
 
-  const item = query.results.find(item => item.id === id);
+  const cache = [...query.results.filter(item => item.id !== mix.current.id), mix.current];
+
+  const item = cache.find(item => item.id === id);
 
   dispatch({
     type: START_PLAYBACK,
