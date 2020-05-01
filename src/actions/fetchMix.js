51f24fdcdee0fabcc5f5b652bc73c0a650da3bfa
Marc Dingena <marc.dingena@gmail.com>
2020-05-02 08:15:51 +1000

    Add mix action creator

---
+++ src/actions/fetchMix.js
@@ -0,0 +1,47 @@
+import {
+  DONE_FETCHING_MIX,
+  SET_MIX,
+  START_FETCHING_MIX
+} from '../constants/actionTypes';
+import { ENDPOINTS } from '../constants/api';
+import addTasks from '../actions/addTasks';
+import completeTasks from '../actions/completeTasks';
+import fetchPage from '../api/fetchPage';
+import extractMixData from '../api/extractMixData';
+
+const fetchMix = slug => async (dispatch, getState) => {
+  dispatch(startFetching());
+  dispatch(addTasks(SET_MIX, 1));
+
+  const options = { slug, _embed: true };
+
+  const response = await fetchPage(ENDPOINTS.MIXES, 1, options);
+
+  if (!response.ok) {
+    dispatch(doneFetching(response.statusText));
+    dispatch(completeTasks(SET_MIX, 1));
+    return;
+  }
+
+  const mix = response.resources.map(extractMixData)[0] || {};
+
+  dispatch(setMix(mix));
+  dispatch(doneFetching());
+  dispatch(completeTasks(SET_MIX, 1));
+};
+
+export default fetchMix;
+
+const startFetching = () => ({
+  type: START_FETCHING_MIX
+});
+
+const doneFetching = (statusText = null) => ({
+  type: DONE_FETCHING_MIX,
+  payload: { statusText }
+});
+
+const setMix = result => ({
+  type: SET_MIX,
+  payload: { result }
+});
