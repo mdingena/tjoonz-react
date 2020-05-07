6355b7c111b98f3b13af765d158a069bac8c23fd
Marc Dingena <marc.dingena@gmail.com>
2020-05-07 16:25:57 +1000

    Add sign in action creator

---
+++ src/actions/signIn.js
@@ -0,0 +1,35 @@
+import {
+  DONE_SIGNING_IN,
+  SET_AUTH,
+  START_SIGNING_IN
+} from '../constants/actionTypes';
+import addTasks from '../actions/addTasks';
+import completeTasks from '../actions/completeTasks';
+import { authenticate } from '../api/authentication';
+
+const signIn = (username, password) => async dispatch => {
+  dispatch(startAuthenticating());
+  dispatch(addTasks(SET_AUTH, 1));
+
+  const authentication = await authenticate(username, password);
+
+  if (!authentication.token) {
+    dispatch(doneAuthenticating('Invalid credentials'));
+    dispatch(completeTasks(SET_AUTH, 1));
+    return;
+  }
+
+  dispatch(doneAuthenticating());
+  dispatch(completeTasks(SET_AUTH, 1));
+};
+
+export default signIn;
+
+const startAuthenticating = () => ({
+  type: START_SIGNING_IN
+});
+
+const doneAuthenticating = (statusText = null) => ({
+  type: DONE_SIGNING_IN,
+  payload: { statusText }
+});
