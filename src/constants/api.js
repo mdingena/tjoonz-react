da0aa7014ad3ffbbbd6d29fd274c271e28dadc4b
Marc Dingena <marc.dingena@gmail.com>
2020-05-07 16:16:13 +1000

    Update API endpoints

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,9 +1,12 @@
-export const BASE_URL = 'https://www.tjoonz.com/wp-json/wp/v2';
+export const BASE_URL = 'https://www.tjoonz.com/wp-json';
 
-export const BASE_STREAM_URL = 'https://www.tjoonz.com/beta.php?id=';
+export const BASE_STREAM_URL = 'https://www.tjoonz.com/beta.php?id='; /** @todo */
 
 export const ENDPOINTS = {
-  MIXES: '/posts'
+  COMMENTS: '/wp/v2/comments',
+  JWT: '/jwt-auth/v1/token',
+  JWT_VALIDATION: '/jwt-auth/v1/token/validate',
+  MIXES: '/wp/v2/posts'
 };
 
 export const FACETTED_SEARCH_OPTIONS_TTL = 10800000; // Three hours.
