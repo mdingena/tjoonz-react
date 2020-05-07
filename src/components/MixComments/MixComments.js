61f6d16e50e922fdb84807c745fd32e20ce75607
Marc Dingena <marc.dingena@gmail.com>
2020-05-07 22:59:36 +1000

    Expect comments and id

--- src/components/MixComments/MixComments.js
+++ src/components/MixComments/MixComments.js
@@ -4,18 +4,17 @@ import Comment, { COMMENT_PROPTYPES } from './Comment';
 import PropTypes from 'prop-types';
 import styles from './MixComments.module.css';
 
-const MixComments = ({ comments, id }) => comments
-  ? (
-    <div className={styles.root}>
-      <div className={styles.heading}>Comments</div>
-      <CommentForm id={id} />
-      {comments.map(comment => <Comment key={`comment-${comment.id}`} {...comment} />)}
-    </div>
-  )
-  : null;
+const MixComments = ({ comments, id }) => (
+  <div className={styles.root}>
+    <div className={styles.heading}>Comments</div>
+    <CommentForm id={id} />
+    {comments.map(comment => <Comment key={`comment-${comment.id}`} {...comment} />)}
+  </div>
+);
 
 MixComments.propTypes = {
-  comments: PropTypes.arrayOf(PropTypes.shape(COMMENT_PROPTYPES))
+  comments: PropTypes.arrayOf(PropTypes.shape(COMMENT_PROPTYPES)),
+  id: PropTypes.number.isRequired
 };
 
 export default MixComments;
