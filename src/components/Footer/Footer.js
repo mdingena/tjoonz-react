be2fda0893935cb148c6db9b1a4be1830df8a879
Marc Dingena <marc.dingena@gmail.com>
2020-07-26 22:17:58 +1000

    Add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import styles from './Footer.module.css';
+
+const Footer = ({ links }) => (
+  <footer className={styles.root}>
+    <ul className={styles.list}>
+      {links.map(({ to, text }) => (
+        <li key={to}>
+          <Link className={styles.link} to={to}>
+            {text}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </footer>
+);
+
+Footer.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      to: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.shape({
+          pathname: PropTypes.string.isRequired,
+          state: PropTypes.shape({
+            from: PropTypes.string.isRequired
+          })
+        })
+      ]),
+      text: PropTypes.string.isRequired
+    })
+  )
+};
+
+export default Footer;
