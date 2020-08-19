04eb2bf328420123918dd6ed7cf93e6cbe0ea5a0
Marc Dingena <marc.dingena@gmail.com>
2020-08-19 16:22:22 +1000

    Remove Upload from navigation

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => (
 
         <div className={styles.navigation}>
           <Wrap>
-            <Navigation links={[{ to: '/about/uploading', text: 'Upload' }]} />
+            <Navigation links={[]} />
           </Wrap>
         </div>
 
