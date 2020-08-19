e92ba61274edb854bacd7af32c049ad200834044
Marc Dingena <marc.dingena@gmail.com>
2020-08-19 16:26:07 +1000

    Add getMyPlaylists function to fetch Collections from API

---
+++ src/api/getMyCollections.js
@@ -0,0 +1,16 @@
+import { BASE_URL, ENDPOINTS } from '../constants/api';
+
+const getMyCollections = token => {
+  const options = {
+    method: 'get',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    json: true
+  };
+
+  return window.fetch(`${BASE_URL}${ENDPOINTS.GET_MY_COLLECTIONS}`, options);
+};
+
+export default getMyCollections;
--- src/constants/api.js
+++ src/constants/api.js
@@ -5,6 +5,7 @@ export const BASE_STREAM_URL = 'https://www.tjoonz.com/beta.php?id='; /** @todo 
 export const ENDPOINTS = {
   COMMENTS: '/wp/v2/comments',
   DOWNVOTE: '/tjnz/v1/downvote',
+  GET_MY_COLLECTIONS: '/tjnz/v1/collections',
   JWT: '/jwt-auth/v1/token',
   JWT_VALIDATION: '/jwt-auth/v1/token/validate',
   MIXES: '/wp/v2/posts',
