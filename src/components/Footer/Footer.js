4825015cd1a1fbcf246decd14dea38362ace3195
Marc Dingena <marc.dingena@gmail.com>
2020-08-25 15:42:50 +1000

    Hide footer links for now

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 
 const Footer = ({ links }) => (
   <footer className={styles.root}>
     <ul className={styles.list}>
-      {links.map(({ to, text }) => (
+      {/* links.map(({ to, text }) => (
         <li key={to}>
           <Link className={styles.link} to={to}>
             {text}
           </Link>
         </li>
-      ))}
+      )) */}
       <li>
         {/* eslint-disable react/jsx-no-target-blank */}
-        <a className={styles.link} href='https://www.tjoonz.com/wp-admin/profile.php' target='_blank'>
+        <a className={styles.link} href='https://www.tjoonz.com/system/wp-admin/profile.php' target='_blank'>
           Manage Profile
         </a>
         {/* eslint-enable react/jsx-no-target-blank */}
